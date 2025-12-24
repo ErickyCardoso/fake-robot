d23100fd758a714602f21f0ada05a043cb3df15a
Ericky <erickyjoficial1@gmail.com>
2025-12-23 23:38:18 -0300

    ATUALIZAÇÃO IMPORTANTE, RSRS

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
     const dados = {
-            "ola": "Olá, eu sou o JHINFERENT'AI, um Chat criado pelo Ericky Cardoso, como posso ajuda-lo?",
+            "ola": "Olá, eu sou o JHINFERENT'AI, um Chat criado pelo Ericky Cardoso, como posso ajudá-lo?",
             "hello": "Hello, I am JHINFERENT'AI, a Chatbot made by Ericky Cardoso, how can I help you?",
-            "oi": "Oi, eu sou o JHINFERENT'AI, um Chat criado pelo Ericky Cardoso, como posso ajuda-lo?",
+            "oi": "Oi, eu sou o JHINFERENT'AI, um Chat criado pelo Ericky Cardoso, como posso ajudá-lo?",
             "hi": "Hi, I'm JHINFERENT'AI, a Chatbot made by Ericky Cardoso, how can I help you?", 
-            "oie": "Oie, eu sou o JHINFERENT'AI, um Chat criado pelo Ericky Cardoso, como posso ajuda-lo?",
-            "bom dia": "Bom dia, o sol já nasceu na fazendinha, como posso ajuda-lo?",
+            "oie": "Oie, eu sou o JHINFERENT'AI, um Chat criado pelo Ericky Cardoso, como posso ajudá-lo?",
+            "bom dia": "Bom dia, o sol já nasceu na fazendinha, como posso ajudá-lo?",
             "good morning": "Good morning! The sun has already come up on the little farm, how can I help you?",
-            "boa tarde": "Boa tarde, como vais? Como posso ajuda-lo?",
+            "boa tarde": "Boa tarde, como vais? Como posso ajudá-lo?",
             "good afternoon": "Good Afternoon, I'm JHINFERENT'AI, how can I help you?",
-            "boa noite": "Boa noite, eu sou o JHINFERENT'AI, como posso ajuda-lo?",
+            "boa noite": "Boa noite, eu sou o JHINFERENT'AI, como posso ajudá-lo?",
             "good evening": "Good Evening, I'm JHINFERENT'AI, how can I help you?",
-            "boa madrugada": "Que escuridão... Como posso ajuda-lo?"
+            "boa madrugada": "Que escuridão... Como posso ajudá-lo?"
         };
-        //let input = (prompt("DIGITE:")).toLowerCase();
-        //document.getElementById("textoin").textContent = input;
-        //function resposta () {
-        //    if (input in dados) {
-        //        console.log(dados[input]);
-        //        document.getElementById("textoia").textContent = (dados[input]);
-        //    } else {
-        //        console.log("Mil perdões, mas não consigo te ajudar com isso. Digite 'ajuda' para saber mais."); 
-        //   }
-        //}
-        // resposta();
-     const textarea = document.querySelector('textarea');
-     textarea.addEventListener('input', () => {
+    const textarea = document.querySelector('textarea');
+    const containeria = document.querySelector('.container-ia');
+    let input;
+    function resposta () {
+        if (input in dados) {
+            const msg = document.createElement('div');
+            msg.textContent = dados[input];
+            msg.classList.add('msg');
+            containeria.appendChild(msg);
+        } else {
+            const msg = document.createElement('div');
+            msg.textContent = ("Mil perdões, mas não consigo te ajudar com isso. Digite 'ajuda' para saber mais.");
+            msg.classList.add('msg');
+            containeria.appendChild(msg);
+        }
+    } 
+    document.addEventListener("keydown", function(event) {
+        if (event.key === 'Enter') {
+            input = textarea.value.toLowerCase().trim();
+            textarea.value = '';
+            event.preventDefault();
+            textarea.style.height = '25px';
+            resposta();
+        }
+    });
+    textarea.addEventListener('input', () => {
         if (textarea.value === '') {
             textarea.style.height = '25px';
         } else {
